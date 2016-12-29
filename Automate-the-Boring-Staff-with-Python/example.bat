cf52f9f48827447e2146b23f4ed908ef49072fc0
Md Rashidul Hasan <rashidulhasan777@gmail.com>
2016-12-29 13:01:50 +0600

    Create example.bat

---
+++ Automate-the-Boring-Staff-with-Python/example.bat
@@ -0,0 +1,11 @@
+#@python.exe or pythonw.exe path %*
+#@pause for stay command prompt until press any key
+#file name should be .bat extension
+Example:
+
+
+@python.exe c:\Users\rashi\python\java.py %*
+@pause
+
+@pythonw.exe c:\Users\rashi\python\mcb.pyw %*
+@pause
